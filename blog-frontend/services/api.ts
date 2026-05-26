c95765a83922ce4e847a076f4d3d8a3686f4c20a
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2026-05-26 13:31:06 +0000

    fix run php codespace

--- blog-frontend/services/api.ts
+++ blog-frontend/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  //baseURL: "http://127.0.0.1:8000/api",
+  baseURL:"https://special-telegram-4964xv6xr6v2j9wp-8000.app.github.dev/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
