84c3b8bdb049a2facb4f97e5c2599f783353a248
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2026-05-26 11:14:13 +0000

    add frontend project

---
+++ blog-frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+       <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Blog 🚀
+      </h1>
+    </main>
+    </div>
+  );
+}
